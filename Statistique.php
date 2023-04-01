7f7a0b1023c679a027843fe084f8e860d83a8e58
erwan didi <furiouse693@gmail.com>
2023-04-01 02:03:28 +0200

    ajout de version 2 pour afficher le chiffre d'affaire de chaque client

--- Statistique.php
+++ Statistique.php
@@ -11,10 +11,74 @@ if ($_SESSION == false) {
     redirect('login.php?error=' . LoginError::CONNECTION_FAILED);
 }
 
-// requête pour récuperer le total de chaque facture 
+// Récupération Base de donnée 
+require_once 'bdd-link/bdd-link.php';
 
-// SELECT c.nom_client, SUM(lf.prix_total) AS chiffre_affaire
-// FROM client 
-// JOIN facture  ON c.client_id = f.client_id
-// JOIN ligne_facture lf ON f.numero_facture = lf.id_facture
-// GROUP BY c.client_id;
+//  requête pour récuperer tous les clients de la BDD
+$query = "SELECT * FROM client";
+$stmt = $pdo->prepare($query);
+$stmt->execute();
+
+// Récupération du header et de la NavBar
+require_once 'Layout/header.php';
+require_once 'Layout/navbar.php'; ?>
+
+<h1 class="text-center">Choisir le client dans la liste</h1>
+
+<div class="d-flex justify-content-center mt-5 ">
+    <form action="" method="get" class="d-flex gap-3 w-50">
+        <select name="id" class="form-select" aria-label="Default select example">
+            <option selected>Sélectionner un Client</option>
+            <?php while ($row = $stmt->fetch()) { ?>
+                <option value="<?php echo $row['id']; ?>"><?php echo $row['nom']; ?></option>
+            <?php } ?>
+        </select>
+        <button type="submit" class="btn btn-dark">Rechercher</button>
+    </form>
+</div>
+
+<?php
+
+if (isset($_GET['id'])) {
+
+    // V2
+
+    $client_id = $_GET['id'];
+
+    $query = 'SELECT client.nom, SUM(total) AS total_sum FROM Facture
+                JOIN client ON Facture.client_id = client.id
+                WHERE Facture.client_id = :client_id
+                GROUP BY client.id';
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([
+        'client_id' => $client_id
+    ]);
+
+    while ($row = $stmt->fetch()) {
+        $client_nom =$row['nom'];
+        $total_sum = $row['total_sum'];
+
+        echo "Client : $client_nom, Chiffre d'affaire sur l'année : $total_sum €";
+    }
+
+    //V1 pour afficher les totals de chaque client
+
+    // $client_id = $_GET['id'];
+
+    // $query = "SELECT client_id, SUM(total) as total_sum FROM Facture WHERE client_id = :client_id";
+    // $stmt = $pdo->prepare($query);
+    // $stmt->execute([
+    //     'client_id' => $client_id
+    // ]
+    // );
+
+    // $result = $stmt->fetch();
+    // $total_sum = $result['total_sum'];
+
+    //     // afficher les résultats
+    //     echo "Chiffre d'affaire du client : " . $clientNom. " : ". $total_sum. "€";
+    }
+
+
+
+?>
