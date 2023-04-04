5f0815e1a690a25e3b0e613340a00c845c14e8a0
erwan didi <furiouse693@gmail.com>
2023-04-05 01:02:05 +0200

    ajout sur la page d'accueil d'un tableau de bord

--- Accueil.php
+++ Accueil.php
@@ -15,7 +15,91 @@ require_once 'bdd-link/bdd-link.php';
 require_once 'Layout/header.php';
 require_once 'Layout/navbar.php'; ?>
 
-<h1 class="text-center">BIENVENUE</h1>
+<?php
+// Récupération des dernières factures
+$stmt = $pdo->prepare("SELECT f.numero_facture, f.date_facture, f.total, c.nom 
+        FROM facture f 
+        JOIN client c ON f.client_id = c.id 
+        ORDER BY f.date_facture DESC 
+        LIMIT 10");
+$stmt->execute();
+$factures = $stmt->fetchAll();
+
+// Récupération du chiffre d'affaires total
+$stmt = $pdo->prepare("SELECT SUM(total) AS chiffre_affaires FROM facture");
+$stmt->execute();
+$chiffre_affaires = $stmt->fetchColumn();
+
+// Récupération des clients les plus actifs
+$stmt = $pdo->prepare("SELECT c.nom, COUNT(*) AS nombre_factures, SUM(f.total) AS montant_total 
+        FROM facture f 
+        JOIN client c ON f.client_id = c.id 
+        GROUP BY c.nom 
+        ORDER BY montant_total DESC 
+        LIMIT 10");
+$stmt->execute();
+$clients = $stmt->fetchAll();
+
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center mb-4">Tableau de bord</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <h2 class="mb-3">Dernières factures</h2>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Numéro de facture</th>
+                        <th>Date de facture</th>
+                        <th>Total</th>
+                        <th>Client</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($factures as $facture) { ?>
+                        <tr>
+                            <td><?php echo $facture["numero_facture"]; ?></td>
+                            <td><?php echo $facture["date_facture"]; ?></td>
+                            <td><?php echo $facture["total"]; ?>€</td>
+                            <td><?php echo $facture["nom"]; ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-6">
+            <h2 class="mb-3">Clients les plus actifs</h2>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Client</th>
+                        <th>Nombre de factures</th>
+                        <th>Montant total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($clients as $client) { ?>
+                        <tr>
+                            <td><?php echo $client["nom"]; ?></td>
+                            <td><?php echo $client["nombre_factures"]; ?></td>
+                            <td><?php echo $client["montant_total"]; ?>€</td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <p class="text-center">Chiffre d'affaires total : <?php echo $chiffre_affaires; ?>€</p>
+        </div>
+    </div>
+</div>
 
 <?php
 if (array_key_exists('success', $_GET)) { ?>
--- Layout/navbar.php
+++ Layout/navbar.php
@@ -1,16 +1,18 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="Accueil.php">Accueil</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link" href="Client.php">Client</a>
-        <a class="nav-link" href="Facture.php">Facture</a>
-        <a class="nav-link" href="Statistique.php">Statistique</a>
-        <a class="nav-link" href="logout.php">Déconnexion</a>
+<div class="d-flex justify-content-around">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="Accueil.php">Accueil</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link" href="Client.php">Client</a>
+          <a class="nav-link" href="Facture.php">Facture</a>
+          <a class="nav-link" href="Statistique.php">Statistique</a>
+          <a class="nav-link" href="logout.php">Déconnexion</a>
+        </div>
       </div>
     </div>
-  </div>
-</nav>
+  </nav>
+</div>
