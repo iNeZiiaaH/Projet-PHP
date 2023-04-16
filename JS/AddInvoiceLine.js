31a922b6c2468848a0cfc7bfbd230b0f48cbb02f
erwan didi <furiouse693@gmail.com>
2023-04-16 11:33:57 +0200

    ajout de commentaire dans le js

--- JS/AddInvoiceLine.js
+++ JS/AddInvoiceLine.js
@@ -3,7 +3,7 @@ function ajoutLigneFacture() {
     var nouvelleLigneFacture = document.createElement("div");
     nouvelleLigneFacture.classList.add("row", "row-cols-3");
 
-    var index = lignesFacture.childElementCount; // on obtient le nombre de ligne actuelle 
+    var index = lignesFacture.childElementCount; // on obtient le nombre de ligne actuel
     var labels = ["Description", "Quantité", "Prix unitaire"];
     var id = ["description", "quantite", "prix_unitaire"];
 
@@ -11,6 +11,7 @@ function ajoutLigneFacture() {
         var colDiv = document.createElement("div");
         colDiv.classList.add("col");
 
+        // Je vien crée les labels
         var label = document.createElement("label");
         label.classList.add("form-label");
         label.textContent = labels[i] + " :";
@@ -21,12 +22,13 @@ function ajoutLigneFacture() {
         input.type = i === 0 ? "text" : "number";
         // je configure l'attribut name, toLowerCase() = id est convertie en minuscule , il évite les problème de casse
         input.name = "lignes_facture[" + index + "][" + id[i].toLowerCase().replace("  ", "_") + "]";
-        input.min = i === 1 ? 1 : 0;
+        input.min = i === 1 ? 1 : 0; // opérateur terniare , renvoie faux si i n'est pas égale a 1
         input.required = true;
         input.id = id[i];
         input.onchange = updatePrixTotal;
         input.step = "0.01";
 
+        // on crée les nouvelles lignes
         colDiv.appendChild(label);
         colDiv.appendChild(input);
         nouvelleLigneFacture.appendChild(colDiv);
--- JS/UpdatePrixTotal.js
+++ JS/UpdatePrixTotal.js
@@ -2,6 +2,7 @@ function updatePrixTotal() {
     var quantites = document.querySelectorAll("#quantite");
     var prix_unitaires = document.querySelectorAll("#prix_unitaire");
     var prix_total = 0;
+    
     for (i = 0; i < quantites.length; i++) {
         prix_total += quantites[i].value * prix_unitaires[i].value;
     }
